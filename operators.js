816996878cf9ba8272946f7693fb5f0e908d5b9d
Anuj Singh <anujsingj05@gmail.com>
2024-11-21 21:27:33 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,25 @@
+var a=10;
+var b=10;
+console.log("Arithmetic Operator:");
+console.log("Addition\t"+(a+b))
+console.log("Subtraction"+(a-b))
+console.log("Multiplication"+(a*b))
+console.log("Divison"+(a/b))
+console.log("Modulus"+(a%b))
+
+var x=10
+var y=4
+console.log("Comparison Operators")
+console.log("Equal\t"+(x==y))
+console.log("Not Equal"+(x!=y))
+console.log("Greater Than"+(x>y))
+console.log("Less Than"+(x<y))
+console.log("Strict Equal"+(x===10))
+console.log("Strict Not Equal"+(x!=='10'))
+
+var a=true
+var b=false
+console.log("Logical Operators:")
+console.log("Logical AND"+(a&&b))
+console.log("Logical OR"+(a || b))
+console.log("Logical NOT"+(!a))
